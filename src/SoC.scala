0bdfa6e90c7373de69c3d161f91107ce8f96fa3d
Zihao Yu <yuzihao@ict.ac.cn>
2025-09-04 12:02:05 +0800

    device: add APB4WDG

--- src/SoC.scala
+++ src/SoC.scala
@@ -42,6 +42,7 @@ class ysyxSoCASIC(implicit p: Parameters) extends LazyModule {
   val lspi      = LazyModule(new APBSPI      (AddrSpace(0x10001000, 0x20)   ++     // SPI controller
                                               AddrSpace(0x30000000, 0x10000000)))  // XIP flash
   val lrtc      = LazyModule(new APB4RTC     (AddrSpace(0x10004000, 0x20)))
+  val lwdg      = LazyModule(new APB4WDG     (AddrSpace(0x10005000, 0x20)))
   val larchinfo = LazyModule(new APB4ArchInfo(AddrSpace(0x10006000, 0x10)))
 
   // interface
@@ -78,7 +79,7 @@ class ysyxSoCASIC(implicit p: Parameters) extends LazyModule {
   val lpsram    = if (Config.hasHomeWork) Some(LazyModule(new APBPSRAM   (AddrSpace(0xa0000000L, 0x400000)))) else None
 
   List(lclint, lplic,
-       lspi, luart0, lrtc, larchinfo,
+       lspi, luart0, lrtc, lwdg, larchinfo,
        lgpio0, lgpio1, lgpio2, luart1, li2c, lps2, lpwm0, lpwm1, ltim0, ltim1, ltim2, ltim3,
        lqspi, li2s,
        lrng, lcrc
@@ -140,6 +141,7 @@ class ysyxSoCASIC(implicit p: Parameters) extends LazyModule {
     }
     lrtc.module.extra.rtc_clk_i := clock_half
     lrtc.module.extra.rtc_rst_n_i := !reset.asBool
+    lwdg.module.extra.rtc_clk_i := clock_half
 
     val i2c_io = li2c.module.extra
     val i2c_scl = IO(Analog(1.W))
