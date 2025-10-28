4ade8ba48a8ae9feb2c0e2f7f3d5778058908be6
Qiming Chu <cchuqiming@gmail.com>
2025-10-28 23:07:08 +0800

    refactor(SOC): update QSPI interface from analog to digital sigs
    * Replace analog QSPI DIO interface with separate digital
    input/output/enable signals
    * Remove TriStateInBuf logic and simplify QSPI input connection, this
    will be connected in soc top
    
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- src/SoC.scala
+++ src/SoC.scala
@@ -166,10 +166,16 @@ class ysyxSoCASIC(implicit p: Parameters) extends LazyModule {
     val qspi_io = lqspi.module.extra
     val qspi_sck_o = IO(Output(Bool()))
     val qspi_nss_o = IO(Output(UInt(4.W)))
-    val qspi_dio  = IO(Vec(4, Analog(1.W)))
+    val qspi_en_o = IO(Output(UInt(4.W)))
+    val qspi_out_o = IO(Output(UInt(4.W)))
+    val qspi_in_i = IO(Input(UInt(4.W)))
     qspi_sck_o := qspi_io.spi_sck_o
     qspi_nss_o := qspi_io.spi_nss_o
-    qspi_io.spi_io_in_i := Cat((0 to 3).map(i => TriStateInBuf(qspi_dio(i), qspi_io.spi_io_out_o(i), qspi_io.spi_io_en_o(i))).reverse)
+    qspi_en_o := qspi_io.spi_io_en_o
+    qspi_out_o := qspi_io.spi_io_out_o
+    qspi_io.spi_io_in_i := qspi_in_i
+    //val qspi_dio  = IO(Vec(4, Analog(1.W)))
+    //qspi_io.spi_io_in_i := Cat((0 to 3).map(i => TriStateInBuf(qspi_dio(i), qspi_io.spi_io_out_o(i), qspi_io.spi_io_en_o(i))).reverse)
 
     val psram_io = lpsram.module.extra
     val psram_sck = IO(Output(Bool()))
@@ -300,6 +306,7 @@ class ysyxSoCFull(implicit p: Parameters) extends LazyModule {
 
     } else {
       masic.psram_in_i := DontCare
+      masic.qspi_in_i := DontCare
     }
 
     val externalPins = IO(new Bundle{
